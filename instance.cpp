a0c2b85646165f16d8048f6412f057b1a01c1c46
jantechner <jantechner@live.com>
2017-11-23 08:31:15 +0100

    Update instance.cpp

--- instance.cpp
+++ instance.cpp
@@ -1,4 +1,3 @@
-#include "stdafx.h"
 #include<cstdlib>
 #include<iostream>
 #include<random>
@@ -39,14 +38,15 @@ double distance(Punkt point1, Punkt point2) {
     return sqrt(pow(point1.getX() - point2.getX(), 2) + pow(point1.getY() - point2.getY(), 2));
 }
 
-Punkt* generateTable(long int size, long int range) {
-	Punkt* table = new Punkt[size];
+void generateTable(Punkt* table, long int size, long int range) {
+
 	Punkt point;
 	long int randX, randY;
 	int generated = 0;
 	random_device random;
 	mt19937 gen(random());
 	uniform_int_distribution<> distribution(0, range);
+
 	for (int i = 0; i < size; i++) {
 		randX = distribution(gen);
 		randY = distribution(gen);
@@ -60,11 +60,12 @@ Punkt* generateTable(long int size, long int range) {
 
 		point.setX(distribution(gen));
 		point.setY(distribution(gen));
+        point.setNumber(i);
+        point.setVisited(false);
 
 		table[i] = point;
 		generated++;
 	}
-	return table;
 }
 
 void printTable(Punkt tab[], long int size) {
@@ -106,12 +107,7 @@ long int getInstanceSize(string fileName) {
 	return size;
 }
 
-<<<<<<< HEAD
-Punkt* readTable(string fileName, long int size) {
-	int x, y, numer;
-=======
 void readTable(string fileName, Punkt* table) {
->>>>>>> ede0e9ce3ee051e66ae6e319d125bf189a4e992b
 	ifstream in(fileName);
 	long int size;
 	in >> size;
@@ -227,7 +223,6 @@ double generateRoute(Punkt tab[], long int* route, long int size, bool printPath
 
 	return dist;
 }
-
 
 
 
